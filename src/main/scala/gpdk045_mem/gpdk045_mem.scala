3673816a17e3c73db406e7151a4d92e52f7255b8
aghosh <agnimesh.ghosh@aalto.fi>
2024-04-17 17:31:47 +0300

    Initial commit

--- src/main/scala/gpdk045_mem/gpdk045_mem.scala
+++ src/main/scala/gpdk045_mem/gpdk045_mem.scala
@@ -9,45 +9,60 @@ import chisel3._
 import dsptools.{DspTester, DspTesterOptionsManager, DspTesterOptions}
 import dsptools.numbers._
 import breeze.math.Complex
+import scala.math._
 
-class gpdk045_mem_io[T <:Data](proto: T,n: Int)
+class gpdk045_mem_io[T <:Data](proto: T,mem_size: Int)
    extends Bundle {
-        val A       = Input(Vec(n,proto))
-        val B       = Output(Vec(n,proto))
+        val AR       = Input(UInt(log2ceil(mem_size).W))
+        val AW       = Input(UInt(log2ceil(mem_size).W))
+        val DW       = Input(proto)
+        val QR       = Output(proto)
         override def cloneType = (new gpdk045_mem_io(proto.cloneType,n)).asInstanceOf[this.type]
    }
 
-class gpdk045_mem[T <:Data] (proto: T,n: Int) extends Module {
-    val io = IO(new gpdk045_mem_io( proto=proto, n=n))
-    val register=RegInit(VecInit(Seq.fill(n)(0.U.asTypeOf(proto.cloneType))))
-    register:=io.A
-    io.B:=register
+class gpdk045_mem[T <:Data] (proto: T,mem_size: Int) extends Module {
+    val io = IO(new gpdk045_mem_io( proto=proto, mem_size=mem_size))
+    
+    val write_val=RegInit(0.U.asTypeOf(proto.cloneType))
+    val mem =SyncReadMem(memsize, proto.cloneType)
+    val write_addr =RegInit(0.U(log2Ceil(memsize).W))
+    val read_addr =RegInit(0.U(log2Ceil(memsize).W))
+    val read_val =RegInit(0.U.asTypeOf(proto.cloneType))
+    write_addr:=io.AW
+    write_val:=io.DW
+    read_addr:=io.AR
+    
+    mem.write(write_addr,write_val)
+
+    read_val:=mem.read(read_addr)
+    
+    io.QR:=read_val
 }
 
 //This gives you verilog
 object gpdk045_mem extends App {
     chisel3.Driver.execute(args, () => new gpdk045_mem(
-        proto=DspComplex(UInt(16.W),UInt(16.W)), n=8)
+        proto=UInt(16.W), memsize=scala.math.pow(2,13).toInt )
     )
 }
 
 //This is a simple unit tester for demonstration purposes
 class unit_tester(c: gpdk045_mem[DspComplex[UInt]] ) extends DspTester(c) {
 //Tests are here 
-    poke(c.io.A(0).real, 5)
-    poke(c.io.A(0).imag, 102)
-    step(5)
+    poke(c.io.AW, 0)
+    poke(c.io.DW, 2)
+    step(1)
+    poke(c.io.AR, 0)
     fixTolLSBs.withValue(1) {
-        expect(c.io.B(0).real, 5)
-        expect(c.io.B(0).imag, 102)
+        expect(c.io.QR, 2)
     }
 }
 
 //This is the test driver 
 object unit_test extends App {
     iotesters.Driver.execute(args, () => new gpdk045_mem(
-            proto=DspComplex(UInt(16.W),UInt(16.W)), n=8
-        )
+            proto=UInt(16.W), memsize=scala.math.pow(2,13).toInt
+          )
     ){
             c=>new unit_tester(c)
     }
